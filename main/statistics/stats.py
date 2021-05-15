886221299a7f1348b64373ff8b0cdaad9d645155
Mert <57871683+mekaan@users.noreply.github.com>
2021-05-15 11:47:45 +0300

    statistics fot graphs

--- main/statistics/stats.py
+++ main/statistics/stats.py
@@ -2,19 +2,22 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import collections
+import numpy as np
 
 
 # %%
 G = nx.Graph()
 #Building graph
-name = input("Pls enter name of the project: ")
-f= open("../../graphs/"+name+"_auther_file.txt","r")
+#name = input("Pls enter name of the project: ")
+#f= open("../../graphs/"+name+"_auther_file.txt","r")
+f= open("momo.edges","r")
+name="fb-pages-food"
 for x in f:
-    y = x.split("#")
+    y = x.split(" ")
     G.add_node(y[0],type="auther")
     G.add_node(y[1],type="file")
     G.add_edge(*y)
-
+print(nx.info(G))
 
 #%%
 #Page rank
@@ -25,18 +28,42 @@ json.dump(pr, f)
 
 
 # %%
-degree_dict = G.degree()
+#Degree frequency
+m=1
+degree_freq = nx.degree_histogram(G)
+degrees = range(len(degree_freq))
+plt.figure(figsize=(12, 5)) 
+plt.loglog(degrees[m:], degree_freq[m:],'go-') 
+plt.xlabel('Degree')
+plt.ylabel('Frequency')
+
+
+#%%
+#Distribution of Node Linkages (degree(log-log scale))
+def plot_degree_histogram(g, normalized=True):
+    print("Creating histogram...")
+    aux_y = nx.degree_histogram(g)
+    
+    aux_x = np.arange(0,len(aux_y)).tolist()
+    
+    n_nodes = g.number_of_nodes()
+    
+    if normalized:
+        for i in range(len(aux_y)):
+            aux_y[i] = aux_y[i]/n_nodes
+
+    plt.title('\nDistribution Of Node Linkages (log-log scale)')
+    plt.xlabel('Degree\n(log scale)')
+    plt.ylabel('Number of Nodes\n(log scale)')
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.plot(aux_x, aux_y, 'o')
+
+plot_degree_histogram(G)
+
 
 # %%
-def plot_degree_dist(G):
-    degrees = [G.degree(n) for n in G.nodes()]
-    plt.hist(degrees)
-    plt.show()
-
-plot_degree_dist(G)
-
-
-# %%
+#Degree distibution function of networkx
 degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
 degreeCount = collections.Counter(degree_sequence)
 deg, cnt = zip(*degreeCount.items())
@@ -55,5 +82,25 @@ plt.axes([0.4, 0.4, 0.5, 0.5])
 Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
 pos = nx.spring_layout(G)
 plt.axis("off")
-nx.draw_networkx_nodes(G, pos, node_size=3)
+nx.draw_networkx_nodes(G, pos, node_size=1)
 nx.draw_networkx_edges(G, pos, alpha=0.2)
+
+
+# %%
+#The diameter helps us understand how wide the graph is.
+nx.diameter(G)
+#for all the different degrees that occur in the graph, it gives the average of the average_neighbor_degree of all nodes with the same degree. 
+#It is a measure of how connected nodes with certain degrees are.
+##The k-mean degree connectivity is the average of the mean neighbor degrees of vertices of degree k.
+nx.average_degree_connectivity(G)
+#The average shortest path length is the sum of path lengths d(u,v) between all pairs of nodes 
+# (assuming the length is zero if v is not reachable from v) normalized by n*(n-1) where n is the number of nodes in G.
+nx.average_shortest_path_length(G)
+
+
+#%%
+#Doesn't work for me
+ucgenler=nx.triangles(G).values()
+number_of_triangles = sum(ucgenler)/3
+print(number_of_triangles)
+
